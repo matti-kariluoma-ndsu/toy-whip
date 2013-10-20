d9ef985035711bff25bbee132544d91a77e17be8
Matti Kariluoma <matti@kariluo.ma>
2013-10-19 21:02:29 -0500

    added templates and statics

--- whip_data/views.py
+++ whip_data/views.py
@@ -3,7 +3,7 @@ from whip.settings import HOME_URL
 from whip_data import forms
 
 def index(request):
-	zipcode_form = forms.SelectLocationByZipcodeForm()
+	zipcode_form = forms.SelectLocationForm()
 	return render_to_response(
 			'main.html', 
 			{ 
@@ -18,25 +18,6 @@ def zipcode_view(request, locations):
 			'view_report.html',
 			{
 				'home_url': HOME_URL,
-				'hidden_zipcode_form': hidden_zipcode_form,
-				'zipcode_get_string': '?%s' % (urlencode( [('zipcode', zipcode)] )),
-				'zipcode': zipcode,
-				'scope_get_string': '&%s' % (urlencode( [('scope', scope)] )),
-				'scope': scope,
-				'not_location_get_string': '&%s' % (urlencode([('not_location', l) for l in not_locations])),
-				'not_locations': not_locations,
-				'variety_get_string': '&%s' % (urlencode([('variety', v) for v in varieties])),
-				'varieties': varieties,
-				'year_get_string': '&%s' % (urlencode( [('year', curyear)] )),
-				'year_url_bit': year_url_bit,
-				'curyear': curyear,
-				'maxyear': maxyear,
-				'page': page,
-				'message': message,
-				'years': years,
-				'blurbs' : unit_blurbs,
-				'curfield' : fieldname,
-				'show_appendix_tables': False,
 			},
 			context_instance=RequestContext(request)
 		)
